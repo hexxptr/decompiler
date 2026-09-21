b5f6459a9591bb67275e61c78753c6c74630a169
hexxptr <leafforge@outlook.com>
2026-09-21 15:18:21 +0200

    Create opcodes.lua

---
+++ src/opcodes.lua
@@ -0,0 +1,145 @@
+local Opcodes = {}
+
+Opcodes.list = {
+    [0]  = {name="NOP",             opmode=0, kmode=0, aux=false},
+    [1]  = {name="BREAK",           opmode=0, kmode=0, aux=false},
+    [2]  = {name="LOADNIL",         opmode=1, kmode=0, aux=false},
+    [3]  = {name="LOADB",           opmode=3, kmode=0, aux=false},
+    [4]  = {name="LOADN",           opmode=4, kmode=0, aux=false},
+    [5]  = {name="LOADK",           opmode=4, kmode=3, aux=false},
+    [6]  = {name="MOVE",            opmode=2, kmode=0, aux=false},
+    [7]  = {name="GETGLOBAL",       opmode=1, kmode=1, aux=true},
+    [8]  = {name="SETGLOBAL",       opmode=1, kmode=1, aux=true},
+    [9]  = {name="GETUPVAL",        opmode=2, kmode=0, aux=false},
+    [10] = {name="SETUPVAL",        opmode=2, kmode=0, aux=false},
+    [11] = {name="CLOSEUPVALS",     opmode=1, kmode=0, aux=false},
+    [12] = {name="GETIMPORT",       opmode=4, kmode=4, aux=true},
+    [13] = {name="GETTABLE",        opmode=3, kmode=0, aux=false},
+    [14] = {name="SETTABLE",        opmode=3, kmode=0, aux=false},
+    [15] = {name="GETTABLEKS",      opmode=3, kmode=1, aux=true},
+    [16] = {name="SETTABLEKS",      opmode=3, kmode=1, aux=true},
+    [17] = {name="GETTABLEN",       opmode=3, kmode=0, aux=false},
+    [18] = {name="SETTABLEN",       opmode=3, kmode=0, aux=false},
+    [19] = {name="NEWCLOSURE",      opmode=4, kmode=0, aux=false},
+    [20] = {name="NAMECALL",        opmode=3, kmode=1, aux=true},
+    [21] = {name="CALL",            opmode=3, kmode=0, aux=false},
+    [22] = {name="RETURN",          opmode=2, kmode=0, aux=false},
+    [23] = {name="JUMP",            opmode=4, kmode=0, aux=false},
+    [24] = {name="JUMPBACK",        opmode=4, kmode=0, aux=false},
+    [25] = {name="JUMPIF",          opmode=4, kmode=0, aux=false},
+    [26] = {name="JUMPIFNOT",       opmode=4, kmode=0, aux=false},
+    [27] = {name="JUMPIFEQ",        opmode=4, kmode=0, aux=true},
+    [28] = {name="JUMPIFLE",        opmode=4, kmode=0, aux=true},
+    [29] = {name="JUMPIFLT",        opmode=4, kmode=0, aux=true},
+    [30] = {name="JUMPIFNOTEQ",     opmode=4, kmode=0, aux=true},
+    [31] = {name="JUMPIFNOTLE",     opmode=4, kmode=0, aux=true},
+    [32] = {name="JUMPIFNOTLT",     opmode=4, kmode=0, aux=true},
+    [33] = {name="ADD",             opmode=3, kmode=0, aux=false},
+    [34] = {name="SUB",             opmode=3, kmode=0, aux=false},
+    [35] = {name="MUL",             opmode=3, kmode=0, aux=false},
+    [36] = {name="DIV",             opmode=3, kmode=0, aux=false},
+    [37] = {name="MOD",             opmode=3, kmode=0, aux=false},
+    [38] = {name="POW",             opmode=3, kmode=0, aux=false},
+    [39] = {name="ADDK",            opmode=3, kmode=2, aux=false},
+    [40] = {name="SUBK",            opmode=3, kmode=2, aux=false},
+    [41] = {name="MULK",            opmode=3, kmode=2, aux=false},
+    [42] = {name="DIVK",            opmode=3, kmode=2, aux=false},
+    [43] = {name="MODK",            opmode=3, kmode=2, aux=false},
+    [44] = {name="POWK",            opmode=3, kmode=2, aux=false},
+    [45] = {name="AND",             opmode=3, kmode=0, aux=false},
+    [46] = {name="OR",              opmode=3, kmode=0, aux=false},
+    [47] = {name="ANDK",            opmode=3, kmode=2, aux=false},
+    [48] = {name="ORK",             opmode=3, kmode=2, aux=false},
+    [49] = {name="CONCAT",          opmode=3, kmode=0, aux=false},
+    [50] = {name="NOT",             opmode=2, kmode=0, aux=false},
+    [51] = {name="MINUS",           opmode=2, kmode=0, aux=false},
+    [52] = {name="LENGTH",          opmode=2, kmode=0, aux=false},
+    [53] = {name="NEWTABLE",        opmode=2, kmode=0, aux=true},
+    [54] = {name="DUPTABLE",        opmode=4, kmode=3, aux=false},
+    [55] = {name="SETLIST",         opmode=3, kmode=0, aux=true},
+    [56] = {name="FORNPREP",        opmode=4, kmode=0, aux=false},
+    [57] = {name="FORNLOOP",        opmode=4, kmode=0, aux=false},
+    [58] = {name="FORGLOOP",        opmode=4, kmode=8, aux=true},
+    [59] = {name="FORGPREP_INEXT",  opmode=4, kmode=0, aux=false},
+    [60] = {name="FASTCALL3",       opmode=3, kmode=1, aux=true},
+    [61] = {name="FORGPREP_NEXT",   opmode=4, kmode=0, aux=false},
+    [62] = {name="NATIVECALL",      opmode=0, kmode=0, aux=false},
+    [63] = {name="GETVARARGS",      opmode=2, kmode=0, aux=false},
+    [64] = {name="DUPCLOSURE",      opmode=4, kmode=3, aux=false},
+    [65] = {name="PREPVARARGS",     opmode=1, kmode=0, aux=false},
+    [66] = {name="LOADKX",          opmode=1, kmode=1, aux=true},
+    [67] = {name="JUMPX",           opmode=5, kmode=0, aux=false},
+    [68] = {name="FASTCALL",        opmode=3, kmode=0, aux=false},
+    [69] = {name="COVERAGE",        opmode=5, kmode=0, aux=false},
+    [70] = {name="CAPTURE",         opmode=2, kmode=0, aux=false},
+    [71] = {name="SUBRK",           opmode=3, kmode=7, aux=false},
+    [72] = {name="DIVRK",           opmode=3, kmode=7, aux=false},
+    [73] = {name="FASTCALL1",       opmode=3, kmode=0, aux=false},
+    [74] = {name="FASTCALL2",       opmode=3, kmode=0, aux=true},
+    [75] = {name="FASTCALL2K",      opmode=3, kmode=1, aux=true},
+    [76] = {name="FORGPREP",        opmode=4, kmode=0, aux=false},
+    [77] = {name="JUMPXEQKNIL",     opmode=4, kmode=5, aux=true},
+    [78] = {name="JUMPXEQKB",       opmode=4, kmode=5, aux=true},
+    [79] = {name="JUMPXEQKN",       opmode=4, kmode=6, aux=true},
+    [80] = {name="JUMPXEQKS",       opmode=4, kmode=6, aux=true},
+    [81] = {name="IDIV",            opmode=3, kmode=0, aux=false},
+    [82] = {name="IDIVK",           opmode=3, kmode=2, aux=false},
+    [83] = {name="GETUDATAKS",      opmode=3, kmode=1, aux=true},
+    [84] = {name="SETUDATAKS",      opmode=3, kmode=1, aux=true},
+    [85] = {name="NAMECALLUDATA",   opmode=3, kmode=1, aux=true},
+    [86] = {name="NEWCLASSMEMBER",  opmode=3, kmode=1, aux=true},
+    [87] = {name="CALLFB",          opmode=3, kmode=0, aux=true},
+    [88] = {name="CMPPROTO",        opmode=4, kmode=0, aux=true},
+    [89] = {name="FASTPCALL",       opmode=3, kmode=0, aux=false},
+    [90] = {name="NEWCLASS",        opmode=3, kmode=1, aux=true},
+}
+
+function Opcodes.get(op)
+    return Opcodes.list[op]
+end
+
+function Opcodes.isAux(op)
+    local info = Opcodes.list[op]
+    return info and info.aux or false
+end
+
+function Opcodes.getOpLength(op)
+    local info = Opcodes.list[op]
+    if not info then return 1 end
+    return info.aux and 2 or 1
+end
+
+function Opcodes.isJump(op)
+    local name = Opcodes.list[op] and Opcodes.list[op].name
+    if not name then return false end
+    
+    
+    return name == "JUMP" or name == "JUMPBACK" or name == "JUMPIF" 
+        or name == "JUMPIFNOT" or name == "JUMPIFEQ" or name == "JUMPIFLE"
+        or name == "JUMPIFLT" or name == "JUMPIFNOTEQ" or name == "JUMPIFNOTLE"
+        or name == "JUMPIFNOTLT" or name == "FORNPREP" or name == "FORNLOOP"
+        or name == "FORGPREP" or name == "FORGLOOP" or name == "FORGPREP_INEXT"
+        or name == "FORGPREP_NEXT" or name == "JUMPXEQKNIL" or name == "JUMPXEQKB"
+        or name == "JUMPXEQKN" or name == "JUMPXEQKS" or name == "CMPPROTO"
+end
+
+function Opcodes.isFastCall(op)
+    local name = Opcodes.list[op] and Opcodes.list[op].name
+    if not name then return false end
+    return name == "FASTCALL" or name == "FASTCALL1" or name == "FASTCALL2"
+        or name == "FASTCALL2K" or name == "FASTCALL3" or name == "FASTPCALL"
+end
+
+function Opcodes.isLoopJump(op)
+    local name = Opcodes.list[op] and Opcodes.list[op].name
+    if not name then return false end
+    return name == "JUMPBACK" or name == "FORGLOOP" or name == "FORNLOOP"
+end
+
+function Opcodes.isFallthrough(op)
+    local name = Opcodes.list[op] and Opcodes.list[op].name
+    if not name then return true end
+    return name ~= "RETURN" and name ~= "JUMP" and name ~= "JUMPBACK" and name ~= "JUMPX"
+end
+
+return Opcodes
